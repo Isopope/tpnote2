d62ad388c10aa7f1de6b512abf170b0253f4e9e4
isopope <nkoubah@gmail.com>
2024-12-12 22:04:41 +0100

    creation de model pour test la datasource

---
+++ src/main/java/fr/univtours/polytech/tpnote2/business/rest/WebApp.java
@@ -0,0 +1,8 @@
+package fr.univtours.polytech.tpnote2.business.rest;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+@ApplicationPath("api")
+public class WebApp extends Application {
+
+}
---
+++ src/main/java/fr/univtours/polytech/tpnote2/models/LivreBean.java
@@ -0,0 +1,58 @@
+package fr.univtours.polytech.tpnote2.models;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+@Entity
+@Table(name = "BOOK")
+public class LivreBean implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "TITLE")
+    private String title;
+
+    @Column(name = "AUTHOR")
+    private String author;
+
+    @Column(name = "FREE")
+    private Boolean free;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Boolean getFree() {
+        return free;
+    }
+
+    public void setFree(Boolean free) {
+        this.free = free;
+    }
+}
